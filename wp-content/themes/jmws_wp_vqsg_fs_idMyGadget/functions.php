89ead1450578dee51b1c91717e7f4474da1bc5b8
Tom W Hartung <tomwhartung@gmail.com>
2015-08-10 14:16:29 -0600

    Figured out how to add a way for the user to select the gadget detector.

--- wp-content/themes/jmws_wp_vqsg_fs_idMyGadget/functions.php
+++ wp-content/themes/jmws_wp_vqsg_fs_idMyGadget/functions.php
@@ -29,4 +29,56 @@ function my_sidebar() {
 /* Add Custom Thumbnail support */
 add_theme_support( 'post-thumbnails' );
 
+function idmygadget_customize_register( $wp_customize ) {
+	//All our sections, settings, and controls will be added here
+	// add_theme_support( 'gadget-detector' );
+
+	//
+	// Winging it based on docos.
+	//
+	$wp_customize->add_section( 'gadget_detector' , array(
+		'title'      => __( 'Gadget Detector', 'jmws_wp_vqsg_fs_idMyGadget' ),
+		'description' => __( 'Test Section Description' ),
+		'priority'   => 30,
+	) );
+
+	$wp_customize->add_setting( 'gadget_detector' , array(
+		'default'     => 'detect_mobile_browsers',
+		'transport'   => 'refresh',
+	) );
+
+	$wp_customize->add_control( 'gadget_detector', array(
+		'label'    => __( 'Copied Label', 'jmws_wp_vqsg_fs_idMyGadget' ),
+		'section'  => 'gadget_detector',
+		'type'     => 'select',
+		'choices'  => array('detect_mobile_browsers','mobile_detect','tera_wurfl'),
+		'priority' => 5,
+	) );
+
+	//
+	// Copied from twentyfourteen theme but heavily modified
+	//
+	$wp_customize->add_section( 'copied_section', array(
+		'title'          => __( 'Copied Section Name', 'jmws_wp_vqsg_fs_idMyGadget' ),
+		'description'    => __( 'Copied Section Description' ),
+		'priority'       => 130,
+		'theme_supports' => 'featured-content',
+	) );
+	$wp_customize->add_setting( 'copied_section', array(
+		'default'           => 'none',
+		'transport'         => 'refresh',
+	) );
+	$wp_customize->add_control( 'copied_section', array(
+		'label'    => __( 'Copied Label', 'jmws_wp_vqsg_fs_idMyGadget' ),
+		'section'  => 'copied_section',
+		'type'     => 'select',
+		'choices'  => array('a','b','c'),
+		'priority' => 1,
+	) );
+
+}
+
+add_action( 'customize_register', 'idmygadget_customize_register' );
+
+
 ?>
