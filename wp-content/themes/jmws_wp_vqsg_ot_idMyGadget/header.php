47edac62370d6a9a50cccb964ef97e0a34ff9f4f
Tom W Hartung <tomwhartung@gmail.com>
2015-09-27 12:46:56 -0600

    Split long line up so I can read it.

--- wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/header.php
+++ wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/header.php
@@ -41,7 +41,9 @@ check_idMyGadget_install();
 			print $jmwsIdMyGadget->getGadgetDetectorStringChar() . '/' . $jmwsIdMyGadget->getGadgetStringChar();
 		?>
 		<div id="header">
-			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+					 title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+					 rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h4><?php bloginfo('description'); ?></h4>
 		</div>
 		<?php
