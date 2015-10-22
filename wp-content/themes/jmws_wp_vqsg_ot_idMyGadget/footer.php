02729a965243a89818401800a5d82bcc8f9dda3c
Tom W Hartung <tomwhartung@gmail.com>
2015-10-22 11:49:46 -0600

    First try at adding the footer nav tool bar.

--- wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/footer.php
+++ wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/footer.php
@@ -1,11 +1,21 @@
 <?php global $jmwsIdMyGadget; ?>
 <div id="footer"
 	<?php echo $jmwsIdMyGadget->jqmDataRole['footer'] . ' ' . $jmwsIdMyGadget->jqmDataThemeAttribute ?>>
+<?php if( has_nav_menu('phone-footer-nav') && $jmwsIdMyGadget->phoneFooterNavThisDevice ) : ?>
+	<nav>
+		<?php
+			wp_nav_menu( array(
+				'theme_location' => 'phone-footer-nav',
+			) );
+		?>
+	</nav>
+<?php endif; ?>
 	<cite>
 	&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url');?>">Matt Beck & Jessica Neuman Beck</a>. All rights reserved.
 	</cite>
  </div>
  <?php wp_footer(); ?>
+
  </div>
  </body>
  </html>
