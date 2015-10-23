5a7140c88634fe1ce57d755d912ff2c7e0cc5013
Tom W Hartung <tomwhartung@gmail.com>
2015-10-22 21:25:11 -0600

    Cleanup (it is working well from what I can tell!).

--- wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/footer.php
+++ wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/footer.php
@@ -7,11 +7,8 @@
 		</nav>
 	<?php endif; ?>
 	<cite>
-	&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url');?>">Matt Beck & Jessica Neumannnnnnnnnnnnnnnn Beck</a>. All rights reserved.
+	&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url');?>">Matt Beck & Jessica Neuman Beck</a>. All rights reserved.
 	</cite>
-	<p>has_nav_menu('phone-header-nav'): <?php echo has_nav_menu('phone-header-nav') ?></p>
-	<p>has_nav_menu('phone-footer-nav'): <?php echo has_nav_menu('phone-footer-nav') ?></p>
-	<p>$jmwsIdMyGadget->phoneFooterNavThisDevice: <?php echo $jmwsIdMyGadget->phoneFooterNavThisDevice ?></p>
  </div>
  <?php wp_footer(); ?>
 
