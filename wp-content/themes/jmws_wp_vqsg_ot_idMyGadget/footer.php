aa46a982c4b6a44c6ace15349024c457d4f38f79
Tom W Hartung <tomwhartung@gmail.com>
2015-10-22 21:15:39 -0600

    Fixed the phone footer nav.

--- wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/footer.php
+++ wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/footer.php
@@ -1,18 +1,17 @@
 <?php global $jmwsIdMyGadget; ?>
 <div id="footer"
 	<?php echo $jmwsIdMyGadget->jqmDataRole['footer'] . ' ' . $jmwsIdMyGadget->jqmDataThemeAttribute ?>>
-<?php if( has_nav_menu('phone-footer-nav') && $jmwsIdMyGadget->phoneFooterNavThisDevice ) : ?>
-	<nav>
-		<?php
-			wp_nav_menu( array(
-				'theme_location' => 'phone-footer-nav',
-			) );
-		?>
-	</nav>
-<?php endif; ?>
+	<?php if( has_nav_menu('phone-footer-nav') && $jmwsIdMyGadget->phoneFooterNavThisDevice ) : ?>
+		<nav data-position="fixed" data-role="navbar">
+			<?php wp_nav_menu( array('theme_location' => 'phone-footer-nav','container' => false) ); ?>
+		</nav>
+	<?php endif; ?>
 	<cite>
-	&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url');?>">Matt Beck & Jessica Neuman Beck</a>. All rights reserved.
+	&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url');?>">Matt Beck & Jessica Neumannnnnnnnnnnnnnnn Beck</a>. All rights reserved.
 	</cite>
+	<p>has_nav_menu('phone-header-nav'): <?php echo has_nav_menu('phone-header-nav') ?></p>
+	<p>has_nav_menu('phone-footer-nav'): <?php echo has_nav_menu('phone-footer-nav') ?></p>
+	<p>$jmwsIdMyGadget->phoneFooterNavThisDevice: <?php echo $jmwsIdMyGadget->phoneFooterNavThisDevice ?></p>
  </div>
  <?php wp_footer(); ?>
 
