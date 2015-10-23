150998e9027bd36124ba1e563870ae9dc6c60ec3
Tom W Hartung <tomwhartung@gmail.com>
2015-10-23 10:26:54 -0600

    Moved footer nav to after the little copyright statement.

--- wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/footer.php
+++ wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/footer.php
@@ -1,14 +1,14 @@
 <?php global $jmwsIdMyGadget; ?>
 <div id="footer"
 	<?php echo $jmwsIdMyGadget->jqmDataRole['footer'] . ' ' . $jmwsIdMyGadget->jqmDataThemeAttribute ?>>
-	<?php if( has_nav_menu('phone-footer-nav') && $jmwsIdMyGadget->phoneFooterNavThisDevice ) : ?>
-		<nav data-position="fixed" data-role="navbar">
-			<?php wp_nav_menu( array('theme_location' => 'phone-footer-nav','container' => false) ); ?>
-		</nav>
-	<?php endif; ?>
 	<cite>
 	&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url');?>">Matt Beck & Jessica Neuman Beck</a>. All rights reserved.
 	</cite>
+	<?php if( has_nav_menu('phone-footer-nav') && $jmwsIdMyGadget->phoneFooterNavThisDevice ) : ?>
+		<nav data-role="navbar" data-position="fixed">
+			<?php wp_nav_menu( array('theme_location' => 'phone-footer-nav','container' => false) ); ?>
+		</nav>
+	<?php endif; ?>
  </div>
  <?php wp_footer(); ?>
 
