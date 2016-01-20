c97f4e80f3bb934f4bb85cdc77aa71d6ec6433d6
Tom W Hartung <tomwhartung@gmail.com>
2016-01-20 15:56:59 -0700

    First pass at adding new class JmwsIdMyGadgetVqsgOt , derived from formerly global functions in functions.php .  Yes you could say this change was inspired by my Drupal work.

---
+++ wp-content/themes/jmws_wp_vqsg_ot_idMyGadget/idMyGadget/JmwsIdMyGadgetVqsgOt.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Functions added to the Our Theme ("ot") theme found in the book, WordPress, a Visual Quick Start Guide ("vqsg")
+ * This code has been added specifically to support device detction
+ *
+ * @author Tom W. Hartung, using the Visual Quick Start Guide's "our-theme" as a starting point
+ * @package Jmws
+ * @since 1.0
+ */
+class JmwsIdMyGadgetWpVqsgOt
+{
+
+	public function __construct()
+	{
+	}
+
+	/**
+	 * Use the $logoTitleDescription to generate the html for the header
+	 */
+	public function getHeaderHtml()
+	{
+		global $jmwsIdMyGadget;
+		$logoTitleDescription = '';
+	
+		if ( $jmwsIdMyGadget->isInstalled() )
+		{
+			$logoTitleDescription = $jmwsIdMyGadget->getLogoTitleDescriptionHtml();
+		}
+		else
+		{
+			$logoTitleDescription = getLogoTitleDescriptionHtml();
+		}
+	
+		$headerHtml  = '';
+		$headerHtml  .= '<header id="header" ' . $jmwsIdMyGadget->jqmDataRole['header'] . ' ';
+		$headerHtml  .= $jmwsIdMyGadget->jqmDataThemeAttribute . '>';
+		$headerHtml  .= $logoTitleDescription;
+		$headerHtml  .= '</header> <!-- #header -->';
+		return $headerHtml;
+	}
+	/**
+	 * If the idMyGadget module is not available we will use this,
+	 * which is the original code downloaded in Sept. 2015
+	 */
+	public function getLogoTitleDescriptionHtml()
+	{
+		$logoTitleDescription = '<h1>' .
+				'<a href="' . esc_url( home_url('/') ) . '" ' .
+				'title="' . esc_attr( get_bloginfo('name','display') ) . '" ' .
+				'rel="home">' . $site_name . '</a></h1>';
+		$logoTitleDescription .= '<h4>' . get_bloginfo('description') . '</h4>';
+		return $logoTitleDescription;
+	}
+}
