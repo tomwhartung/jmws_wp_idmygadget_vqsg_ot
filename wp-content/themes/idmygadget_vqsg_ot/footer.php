bcf044fb24be99f8aa06c98711331981d04c81bd
Tom W Hartung <tomwhartung@gmail.com>
2016-02-12 00:37:14 -0700

    Fixed the footer nav bar.

--- wp-content/themes/idmygadget_vqsg_ot/footer.php
+++ wp-content/themes/idmygadget_vqsg_ot/footer.php
@@ -1,14 +1,14 @@
 <?php global $jmwsIdMyGadget; ?>
-<div id="footer"
-	<?php echo $jmwsIdMyGadget->jqmDataRole['footer'] . ' ' . $jmwsIdMyGadget->jqmDataThemeAttribute ?>>
+<div id="footer" <?php echo $jmwsIdMyGadget->getFooterAttributes() ?>>
+ <?php if( has_nav_menu('phone-footer-nav') && $jmwsIdMyGadget->phoneFooterNavThisDevice ) : ?>
+	<nav data-role="navbar">
+		<?php wp_nav_menu( array('theme_location' => 'phone-footer-nav','container' => false) ); ?>
+	</nav>
+ <?php else: ?>
 	<cite>
 	&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('url');?>">Matt Beck & Jessica Neuman Beck</a>. All rights reserved.
 	</cite>
-	<?php if( has_nav_menu('phone-footer-nav') && $jmwsIdMyGadget->phoneFooterNavThisDevice ) : ?>
-		<nav data-role="navbar" data-position="fixed">
-			<?php wp_nav_menu( array('theme_location' => 'phone-footer-nav','container' => false) ); ?>
-		</nav>
-	<?php endif; ?>
+ <?php endif; ?>
  </div>
  <?php wp_footer(); ?>
 
