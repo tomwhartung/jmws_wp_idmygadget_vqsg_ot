570300538d7d6cd7d6e4752c99778a7750f92cb5
Tom W Hartung <tomwhartung@gmail.com>
2016-02-11 20:48:05 -0700

    Removed "vqsg_ot:" from title.

--- wp-content/themes/idmygadget_vqsg_ot/header.php
+++ wp-content/themes/idmygadget_vqsg_ot/header.php
@@ -13,7 +13,7 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
-	<title>vqsg_ot:
+	<title>
 		<?php bloginfo('name'); ?> <?php wp_title(); ?> 
 	</title>
 	
