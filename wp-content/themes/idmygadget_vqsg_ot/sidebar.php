a94446c84d06ca7d4864993fd0de160b33e85eea
Tom W Hartung <tomwhartung@gmail.com>
2016-03-07 13:33:42 -0700

    Updated sidebar.php to use the new setIncludeSidebar* variables we are now setting in the jmwsIdMyGadget object.

--- wp-content/themes/idmygadget_vqsg_ot/sidebar.php
+++ wp-content/themes/idmygadget_vqsg_ot/sidebar.php
@@ -1,29 +1,5 @@
 <?php
 global $jmwsIdMyGadget;
-$include_sidebar_phones = FALSE;
-$include_sidebar_tablets = FALSE;
-$include_sidebar_desktops = FALSE;
-if ( $jmwsIdMyGadget->isPhone() )
-{
-	if ( is_active_sidebar('sidebar-phones') )
-	{
-		$include_sidebar_phones = TRUE;
-	}
-}
-else if ( $jmwsIdMyGadget->isTablet() )
-{
-	if ( is_active_sidebar('sidebar-tablets') )
-	{
-		$include_sidebar_tablets = TRUE;
-	}
-}
-else
-{
-	if ( is_active_sidebar('sidebar-desktops') )
-	{
-		$include_sidebar_desktops = TRUE;
-	}
-}
 ?>
 
 <div class="sidebar">
@@ -32,15 +8,15 @@ else
 			<?php dynamic_sidebar( 'primary' ); ?>
 		</div>
 	<?php endif; ?>
-	<?php if ( $include_sidebar_phones ) : ?>
+	<?php if ( $jmwsIdMyGadget->includeSidebarPhones ) : ?>
 		<div id="sidebar-phones">
 			<?php dynamic_sidebar( 'sidebar-phones' ); ?>
 		</div>
-	<?php elseif ( $include_sidebar_tablets ) : ?>
+	<?php elseif ( $jmwsIdMyGadget->includeSidebarTablets ) : ?>
 		<div id="sidebar-tablets">
 			<?php dynamic_sidebar( 'sidebar-tablets' ); ?>
 		</div>
-	<?php elseif ( $include_sidebar_desktops ) : ?>
+	<?php elseif ( $jmwsIdMyGadget->includeSidebarDesktops ) : ?>
 		<div id="sidebar-desktops">
 			<?php dynamic_sidebar( 'sidebar-desktops' ); ?>
 		</div>
