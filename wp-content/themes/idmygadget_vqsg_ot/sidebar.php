e6d0a213179c573a406adff86e9139e5fd1a2cd9
Tom W Hartung <tomwhartung@gmail.com>
2016-03-07 13:49:48 -0700

    Moved the call to set the variables to sidebar.php , and updated to print the sidebar only when we know there are widgets that we want to see there.

--- wp-content/themes/idmygadget_vqsg_ot/sidebar.php
+++ wp-content/themes/idmygadget_vqsg_ot/sidebar.php
@@ -1,24 +1,26 @@
 <?php
 global $jmwsIdMyGadget;
+$jmwsIdMyGadget->setIncludeSidebarVariables();
 ?>
-
-<div class="sidebar">
-	<?php if ( is_active_sidebar('primary') ) : ?>
-		<div id="sidebar-primary">
-			<?php dynamic_sidebar( 'primary' ); ?>
-		</div>
-	<?php endif; ?>
-	<?php if ( $jmwsIdMyGadget->includeSidebarPhones ) : ?>
-		<div id="sidebar-phones">
-			<?php dynamic_sidebar( 'sidebar-phones' ); ?>
-		</div>
-	<?php elseif ( $jmwsIdMyGadget->includeSidebarTablets ) : ?>
-		<div id="sidebar-tablets">
-			<?php dynamic_sidebar( 'sidebar-tablets' ); ?>
-		</div>
-	<?php elseif ( $jmwsIdMyGadget->includeSidebarDesktops ) : ?>
-		<div id="sidebar-desktops">
-			<?php dynamic_sidebar( 'sidebar-desktops' ); ?>
-		</div>
-	<?php endif; ?>
-</div>
+<?php if ( $jmwsIdMyGadget->includeSidebar ) : ?>
+	<div class="sidebar">
+		<?php if ( is_active_sidebar('primary') ) : ?>
+			<div id="sidebar-primary">
+				<?php dynamic_sidebar( 'primary' ); ?>
+			</div>
+		<?php endif; ?>
+		<?php if ( $jmwsIdMyGadget->includeSidebarPhones ) : ?>
+			<div id="sidebar-phones">
+				<?php dynamic_sidebar( 'sidebar-phones' ); ?>
+			</div>
+		<?php elseif ( $jmwsIdMyGadget->includeSidebarTablets ) : ?>
+			<div id="sidebar-tablets">
+				<?php dynamic_sidebar( 'sidebar-tablets' ); ?>
+			</div>
+		<?php elseif ( $jmwsIdMyGadget->includeSidebarDesktops ) : ?>
+			<div id="sidebar-desktops">
+				<?php dynamic_sidebar( 'sidebar-desktops' ); ?>
+			</div>
+		<?php endif; ?>
+	</div>
+<?php endif; ?>
